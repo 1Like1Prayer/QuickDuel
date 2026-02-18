1a045deadc6e280c7d8d46ca8db8c4459bca397e
Tamir <tamirraih13@gmail.com>
2026-02-18 03:08:55 +0200

    convert to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,13 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
-
-const config = defineConfig({
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [
-    devtools(),
-    netlify(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
   ],
 })
-
-export default config
